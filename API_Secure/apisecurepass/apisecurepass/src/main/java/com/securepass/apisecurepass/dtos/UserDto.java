0cab9601a1a3fb8550e6cf0330b069fe32d8a3af
Eduardo Alves <46291995867@info.local>
2024-02-01 19:40:25 -0300

    API com controllers finalizados esperando somente requisicao da Face

---
+++ API_Secure/apisecurepass/apisecurepass/src/main/java/com/securepass/apisecurepass/dtos/UserDto.java
@@ -0,0 +1,29 @@
+package com.securepass.apisecurepass.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record UserDto(
+        int matricula, // Número de identificação do usuário
+        String nome, // Nome do usuário
+        String setor, // Área de trabalho ou setor do usuário
+
+        @NotBlank @Email(message = "O email deve estar em um formato válido") String email, // Email do usuário (validado quanto ao formato)
+
+//      @NotBlank String senha, deve ser analisado - Provavelmente um campo para senha (comentado para análise)
+
+        //Formato da Data a ser visto
+        @DateTimeFormat(pattern = "yyyy-MM-dd") Date nascimento , // Data de nascimento do usuário
+
+        String funcao, // Função ou cargo do usuário
+        int sessao, // Número de sessão
+        MultipartFile image, // Arquivo de imagem para o rosto do usuário
+        UUID id_tipousuario // Tipo de usuário (representado por um UUID)
+
+) {
+}
